6e7f4bcc7e00df0b4a6414f659bee1bbcc7db533
Leejaeho1234 <ljh2735294@naver.com>
2024-05-30 00:35:18 +0900

    touchend로 변경

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ length.addEventListener('click', function(){
         alert('오답입니다!');
     }
 })
+length.addEventListener('touchend', function(){
+    const userInput = prompt('length는 몇일까요?');
+    const userNumber = Number(userInput);
+    if(userNumber === 5){
+        alert('정답입니다!');
+    }else{
+        alert('오답입니다!');
+    }
+})
